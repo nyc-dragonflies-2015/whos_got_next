8541ff7285c29e6eb0dfe3a6670ea68fb62c0208
Sheldon Martin <sheld0ri@gmail.com>
2015-07-16 16:06:04 -0400

    Updates User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many: :created_games, foreign_key: :owner_id, class_name: 'Game'
+  has_many: :players
+  has_many: :games, through: :players
 end
