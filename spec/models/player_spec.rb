2da57511a1ce05429309f09195e09585d7bc9433
Sheldon Martin <sheld0ri@gmail.com>
2015-07-16 17:08:28 -0400

    Adds test to Player model spec

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Player do
+  let(:john) { User.create(first_name: 'John', last_name: 'Doe', email: 'john@example.com', phone_number: '555-555-5555', password: '123456', username: 'johndoe') }
+  let(:jane) { User.create(first_name: 'Jane', last_name: 'Doe', email: 'jane@example.com', phone_number: '555-555-5555', password: '123456', username: 'janedoe') }
+  let(:game) { Game.create(sport: 'Basketball', start_time: '2015-07-13 18:35:57', end_time: '2015-07-13 19:35:57', location: 'Central Park', owner_id: jane.id) }
+  let(:player) { Player.create(user_id: john.id, game_id: game.id) }
+  let(:player2) { Player.create(user_id: jane.id, game_id: game.id, attending: true) }
+
+  describe 'game' do
+    it 'should belong to a game' do
+      expect(player).to respond_to :game
+    end
+  end
+
+  describe 'user' do
+    it 'should have many games' do
+      expect(player).to respond_to :user
+    end
+  end
+
+  describe 'attending' do
+    it 'should be able to attend a game' do
+      expect(player2.attending).to be_truthy
+    end
+  end
 end
