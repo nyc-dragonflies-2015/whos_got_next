303cb631b793b1c60f1919d0b637eaff6e2f5871
JMC11 <JMC11614792@gmail.com>
2015-07-16 18:28:02 -0400

    added presence of validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :created_games, foreign_key: :owner_id, class_name: 'Game'
   has_many :players
   has_many :games, through: :players
+
+  validates :email, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
 end
