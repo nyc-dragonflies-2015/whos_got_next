27f8bb3e135f2634116e99fd0bf88acf5d9917c9
JMC11 <JMC11614792@gmail.com>
2015-07-16 17:49:36 -0400

    modified user model tests with shoulda gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,21 +3,7 @@ require 'rails_helper'
 describe User do
   let(:user) { User.create(first_name: 'John', last_name: 'Doe', email: 'john@example.com', phone_number: '555-555-5555', password: '123456', username: 'johndoe') }
 
-  describe 'players' do
-    it 'should have many players' do
-      expect(user).to respond_to :players
-    end
-  end
-
-  describe 'games' do
-    it 'should have many games' do
-      expect(user).to respond_to :games
-    end
-  end
-
-  describe 'created games' do
-    it 'should have many created games' do
-      expect(user).to respond_to :created_games
-    end
-  end
+  it { should have_many(:players) }
+  it { should have_many(:games) }
+  it { should have_many(:created_games) }
 end
