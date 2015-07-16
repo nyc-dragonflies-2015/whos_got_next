a9fe5c2abef88deeee70af6d852d3a00fde81c36
JMC11 <JMC11614792@gmail.com>
2015-07-16 19:43:47 -0400

    added presence of validations to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,6 @@ class Game < ActiveRecord::Base
   belongs_to :owner, class_name: 'User'
   has_many :players
   has_many :users, through: :players
+
+  validates_presence_of :sport, :start_time, :location
 end
