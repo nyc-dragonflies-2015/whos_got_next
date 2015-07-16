082a2e084bd302e768eb917c9b01bef00d586950
JMC11 <JMC11614792@gmail.com>
2015-07-16 18:42:36 -0400

    added tests for user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,10 @@ describe User do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:password) }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+
+  it { should validate_length_of(:password).is_at_least(6) }
+  it { should validate_uniqueness_of(:username) }
+
 end
