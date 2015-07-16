261cd1ab70f0bfbccc56fc857e673b9754040143
Sheldon Martin <sheld0ri@gmail.com>
2015-07-16 16:43:33 -0400

    Adds association test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe User do
+  let(:user) { User.create(first_name: 'John', last_name: 'Doe', email: 'john@example.com', phone_number: '555-555-5555', password: '123456', username: 'johndoe') }
+
+  describe 'players' do
+    it 'should have many players' do
+      expect(user).to respond_to :players
+    end
+  end
+
+  describe 'games' do
+    it 'should have many games' do
+      expect(user).to respond_to :games
+    end
+  end
+
+  describe 'created games' do
+    it 'should have many created games' do
+      expect(user).to respond_to :created_games
+    end
+  end
 end
