5b152610e890a96d2ea5804a1b235fd2d5646a8f
michaelbbozza <michaelbbozza@gmail.com>
2015-07-17 12:05:51 -0400

    new game form, game controller method new

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,25 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
 
 root "games#index"
 
+resources :games
+
+
+
 # resources :users, :games, :players
 
-# get "/login" => "sessions#new"
-# post "/login" => "sessions#create"
-# delete "/logout" => "sessions#destroy"
+get "/login" => "sessions#new"
+post "/login" => "sessions#create"
+delete "/logout" => "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
