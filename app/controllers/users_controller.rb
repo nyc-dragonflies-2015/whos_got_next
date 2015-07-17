ad9574b4b69982840b6827a9678e2f782d0e4e5d
Sheldon Martin <sheld0ri@gmail.com>
2015-07-17 18:06:55 -0400

    Adds ability to show and edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,32 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
+    if @user.save
       redirect_to root_path
     else
-      redirect_to new_user_path
+      render :new
     end
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
