af5f62bd974b14f2fb8e9685cbc6e05a3277704e
michaelbbozza <michaelbbozza@gmail.com>
2015-07-17 14:21:54 -0400

    games controller methods, edit and show templates

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,11 +4,20 @@ class GamesController < ApplicationController
   end
 
   def show
-
+    @game = Game.find_by(id: params[:id])
   end
 
   def new
     @game = Game.new
+  end
+
+  def create
+    game = Game.new(game_params)
+    if game.save
+
+    else
+      redirect_to new_game_path
+    end
   end
 
   def edit
@@ -21,5 +30,11 @@ class GamesController < ApplicationController
 
   def destroy
 
+  end
+
+private
+
+  def game_params
+    params.require(:game).permit(:sport, :start_time, :end_time, :location)
   end
 end
