6bbab9f6e16aca3c62c1ee0cb3a39847f8d41f13
michaelbbozza <michaelbbozza@gmail.com>
2015-07-17 14:40:05 -0400

    added create method.view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,9 +14,9 @@ class GamesController < ApplicationController
   def create
     game = Game.new(game_params)
     if game.save
-
+      redirect_to game_path(game)
     else
-      redirect_to new_game_path
+      render :new
     end
   end
 
