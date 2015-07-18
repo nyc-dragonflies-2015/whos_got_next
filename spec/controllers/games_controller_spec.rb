24be4429d91d899b161d41dc3b71774bf1170e92
JMC11 <JMC11614792@gmail.com>
2015-07-18 13:28:58 -0400

    added template and create tests for game

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,7 +1,143 @@
 require 'rails_helper'
 
-describe 'Games Controller' do
-  User.create(first_name: 'John', last_name: 'Doe', email: 'john@example.com', phone_number: '555-555-5555', password: '123456', username: 'johndoe')
+describe GamesController do
+  let(:user) { User.create(first_name: "mike", last_name: "b", username: "mikeb", password: "123456", email: "mike@example.com", phone_number: "011-345-6789") }
+
+  let(:game) { Game.create(sport: "Basketball", start_time: "2015-7-20 16:00", end_time: "2015-7-20 17:00", location: "Grand Central Park", owner_id: 1) }
+
+  before :each do
+    def user_attributes
+      {first_name: "michael", last_name: "bozza", username: "mikeb", password: "123456", email: "mike@example.com", phone_number: "011-345-6789"}
+    end
+
+    def game_attributes
+      {sport: "Basketball", start_time: "2015-7-20 16:00", end_time: "2015-7-20 17:00", location: "Grand Central Park", owner_id: 1}
+    end
+
+    def game_attributes_missing_sport
+      {first_name: "mike", last_name: "b", password: "123456", email: "mike@example.com", phone_number: "011-345-6789"}
+    end
+  end
+
+  describe "GET #index" do
+    it "renders the games :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+
+    # it "loads all games" do
+    #   game1 = Game.create(sport: "Basketball", start_time: "2015-7-20 16:00", end_time: "2015-7-20 17:00", location: "Battery Park", owner_id: 3)
+    #   game2 = Game.create(sport: "Basketball", start_time: "2015-7-20 16:00", end_time: "2015-7-20 17:00", location: "Bryant Park", owner_id: 2)
+    #   get :index
+    #   expect(assigns(:games)).to match_array([game1, game2])
+    # end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested game to @game" do
+      get :show, id: game
+      expect(assigns(:game)).to eq (game)
+    end
+
+    it "renders the game :show template" do
+      get :show, id: game
+      expect(response).to render_template :show
+    end
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested game to @game" do
+      get :edit, id: game
+      expect(assigns(:game)).to eq game
+    end
+
+    it "renders the games :edit template" do
+      get :edit, id: game
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "saves the new game in the database" do
+        expect {
+          post :create, game: game_attributes
+          }.to change(Game, :count).by(1)
+      end
+    end
+  end
+
+  #   context "with valid attributes" do
+  #     it "does not save the new user to the database" do
+  #       expect {
+  #         post :create, user: user_attributes_missing_username
+  #       }.to_not change(User, :count)
+  #     end
+
+  #     it "does not save the new user to the database" do
+  #       expect {
+  #         post :create, user: user_attributes_invalid_password
+  #       }.to_not change(User, :count)
+  #     end
+  #   end
+  # end
+
+  # describe "PATCH #update" do
+  #   context "valid attributes" do
+  #     it "locates the requested @user" do
+  #       patch :update, id: user, user: user_attributes
+  #       expect(assigns(:user)).to eq(user)
+  #     end
+
+  #     it "changes @user's attributes" do
+  #       patch :update, id: user, user: user_attributes
+
+  #       user.reload
+  #       expect(user.full_name).to eq("Michael Bozza")
+  #     end
+
+  #     it "redirects to the updated user" do
+  #       patch :update, id: user, user: user_attributes
+  #       expect(response).to redirect_to user
+  #     end
+  #   end
+
+  #   context "with invalid attributes" do
+  #     it "does not change the user's attributes" do
+  #       patch :update, id: user, user: user_attributes_invalid_password
+
+  #       user.reload
+  #       expect(user.full_name).to eq("Mike B")
+  #     end
+
+  #     it "re-renders the :edit template" do
+  #       patch :update, id: user, user: user_attributes_invalid_password
+  #       expect(response).to render_template :edit
+  #     end
+  #   end
+
+
+  # end
+
+  # describe 'DELETE #destroy' do
+  #   before :each do
+  #     @user = User.create(user_attributes)
+  #   end
+
+  #   it "deletes the user" do
+  #     expect{
+  #       delete :destroy, id: @user
+  #     }.to change(User,:count).by(-1)
+  #   end
+
+  #   it "redirects to games#index" do
+  #     delete :destroy, id: @user
+  #     expect(response).to redirect_to root_path
+  #   end
+  # end
+end
+
+
 
   # before(:each) do
   #   visit("/")
@@ -21,4 +157,4 @@ describe 'Games Controller' do
   #     expect(page).to have_content("GC Park")
   #   end
   # end
-end
+# end
