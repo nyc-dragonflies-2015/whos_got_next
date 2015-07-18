4f68c3d78db3d8e10ad5d665eea6101fec816f8c
JMC11 <JMC11614792@gmail.com>
2015-07-18 16:00:26 -0400

    added attending methods

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,16 @@
 class Player < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
+
+  def self.attending_count(game_id)
+    Player.where(game_id: game_id, attending: true).count
+  end
+
+  def is_attending?
+    if self.attending
+      return "Attending"
+      else
+      return "Not Attending"
+    end
+  end
 end
