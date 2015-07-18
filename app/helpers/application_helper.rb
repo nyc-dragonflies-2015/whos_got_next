2bc187c8b60c4035d8e75781018604f85e2af063
JMC11 <JMC11614792@gmail.com>
2015-07-18 17:42:38 -0400

    added current user helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
   def is_authenticated?
     return !!session[:user_id]
   end
+
+  def current_user
+    if session[:user_id]
+      return User.find(session[:user_id])
+    else
+      return nil
+    end
+  end
 end
