25ed7a3902bd465ac94ffb7bc3e8b0bc29a60457
Sheldon Martin <sheld0ri@gmail.com>
2015-07-19 18:03:08 -0400

    Updates PlayersController#to_boolean
    PlayersController#to_boolean is now private and a class method

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,13 +1,15 @@
 class PlayersController < ApplicationController
   def status
     player = Player.find(params[:id])
-    player.attending = to_boolean(params[:status])
+    player.attending = PlayersController.to_boolean(params[:status])
     player.save
 
     redirect_to user_path(params[:user_id])
   end
 
-  def to_boolean(string)
+  private
+
+  def self.to_boolean(string)
     case string.downcase
     when 'true'
       return true
