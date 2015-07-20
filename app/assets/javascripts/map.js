3452c0dc19fa65c70c6ad8df63a4f235cba8303b
michaelbbozza <michaelbbozza@gmail.com>
2015-07-20 16:34:30 -0400

    fixed indentation

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,19 +3,24 @@ var placeSearch, autocomplete, getDestination, map, pos, center;
 function initialize() {
 
   if (navigator.geolocation) {
+
     navigator.geolocation.getCurrentPosition(function(position) {
-    center = new google.maps.LatLng(
-            position.coords.latitude, position.coords.longitude);
+
+    center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
     var mapOptions = {
       zoom: 15,
       center: center,
       mapTypeId:google.maps.MapTypeId.ROADMAP
     };
-      map = new google.maps.Map(document.getElementById("googleMap"),mapOptions);
-      var marker=new google.maps.Marker({
-        position: center,
-        });
-      marker.setMap(map);
+
+    map = new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+    var marker=new google.maps.Marker({
+      position: center,
+      });
+
+    marker.setMap(map);
     });
   }
 
@@ -29,19 +34,27 @@ function initialize() {
 function geolocate() {
 
   var destination = $('#autocomplete').val();
+
   if (navigator.geolocation) {
+
     navigator.geolocation.getCurrentPosition(function(position) {
-      pos = new google.maps.LatLng(
-          position.coords.latitude, position.coords.longitude);
+
+      pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
       var directionsService = new google.maps.DirectionsService();
+
       var directionsDisplay = new google.maps.DirectionsRenderer();
+
       directionsDisplay.setMap(map);
+
       directionsDisplay.setPanel(document.getElementById('map-canvas'));
+
       var request = {
         origin: pos,
         destination: destination,
         travelMode: google.maps.DirectionsTravelMode.TRANSIT
       };
+
      directionsService.route(request, function (response, status) {
          if (status == google.maps.DirectionsStatus.OK) {
            directionsDisplay.setDirections(response);
