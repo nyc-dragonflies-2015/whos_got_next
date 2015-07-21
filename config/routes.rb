cd8dd58070898a9dcb10a83445c13e8a447e2fa9
michaelbbozza <michaelbbozza@gmail.com>
2015-07-21 16:01:38 -0400

    added new route and fixed secrets file

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,5 @@ Rails.application.routes.draw do
 
   patch "/players/status" => "players#status", as: "status"
 
-  post 'twilio/voice' => 'twilio#voice'
-  get 'notifications/notify' => 'notifications#notify'
+  get 'games/:id/invites' => 'users#send_text'
 end
