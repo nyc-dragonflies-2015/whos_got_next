29ac280c2785723024fdcf99ae5c4d9b69131f1f
michaelbbozza <michaelbbozza@gmail.com>
2015-07-22 10:42:19 -0400

    added conditional to remove button if game is private and not invited

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,5 +1,7 @@
 module GamesHelper
   def has_satisfactory_conditions?(game)
-    game.still_active? && Game.max_players_allowed(game.id)
+    if game.still_active? && Game.max_players_allowed(game.id) &&
+      (game.status == false || game.invites.find_by(id: session[:user_id]))
+    end
   end
 end
