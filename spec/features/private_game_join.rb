2376c02765d56c44cff5788ee41e12aedee1891c
JMC11 <JMC11614792@gmail.com>
2015-07-22 15:42:44 -0400

    tests pass for private games

--- spec/features/private_game_join.rb
+++ spec/features/private_game_join.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+  include GamesHelper
 
   describe 'the joining/leaving a private game proccess', type: :feature do
 
@@ -6,7 +7,7 @@ require 'rails_helper'
       User.create(first_name: 'John', last_name: 'Doe', email: 'john@example.com', phone_number: '555-555-5555', password: '123456', username: 'johndoe')
       User.create(first_name: 'Jane', last_name: 'Doe', email: 'jane@example.com', phone_number: '123-456-6789', password: '123456', username: 'janedoe')
       Game.create(sport: 'Basketball', start_time: '2015-07-28 18:35:57', end_time: '2015-07-28 19:35:57', location: 'Central Park', owner_id: 1, private_game: true)
-      Player.create(user_id: User.last.id, game_id: game.last.id)
+      Player.create(user_id: User.last.id, game_id: Game.last.id)
 
       visit root_path
       click_link 'Login'
@@ -16,8 +17,33 @@ require 'rails_helper'
       visit root_path
     end
 
-    scenario 'a player can join a private game if invited' do
-      expect(Game.last.status).to eq('private')
+    scenario 'game should be private' do
+      expect(Game.first.is_game_private?(Game.first)).to eq('Private')
       expect(page).to have_content('Status: Not Attending')
+    end
+
+    scenario 'user can join private game if invited' do
+      click_button 'Join'
+      expect(Player.attending_count(Game.first)).to eq(1)
+      expect(page).to have_content('Status: Attending')
+    end
+
+    scenario 'user can leave a game after joining' do
+      click_button 'Join'
+      click_button 'Leave'
+      expect(Player.attending_count(Game.first)).to eq(0)
+      expect(page).to have_content('Status: Not Attending')
+    end
+
+    scenario 'user should not see private game if not invited' do
+      click_link 'Logout'
+      visit root_path
+      click_link 'Login'
+      fill_in 'Username', with: 'johndoe'
+      fill_in 'Password', with: '123456'
+      click_button 'Login'
+      visit root_path
+
+      expect(page).to have_no_content('Private')
     end
   end
