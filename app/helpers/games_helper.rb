964db77c55cd81cbeaa40f2cb7d2fde0c2a4efaa
michaelbbozza <michaelbbozza@gmail.com>
2015-07-22 11:28:49 -0400

    created new method to test if game is private

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,17 @@
 module GamesHelper
   def has_satisfactory_conditions?(game)
-    if game.still_active? && Game.max_players_allowed(game.id) &&
-      (game.status == false || game.invites.find_by(id: session[:user_id]))
+    if game.still_active? && Game.max_players_allowed(game.id) && (game.private_game || game.invites.find_by(id: session[:user_id]))
+      return true
+    else
+      return false
+    end
+  end
+
+  def is_game_private?(game)
+    if game.private_game == true
+      "Private"
+    else
+      "Public"
     end
   end
 end
